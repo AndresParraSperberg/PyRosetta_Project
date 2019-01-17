ac691ab2cfe94a307389a3c46f5c5533fe9dc341
Alican Gülsevin <alican@Alicans-MacBook-Air.local>
2019-01-17 15:31:39 -0500

    Improved the residue selectors to cover the antigen interface as well and added comments.

--- residue_selector_alican.py
+++ residue_selector_alican.py
@@ -2,7 +2,11 @@
 from pyrosetta import *
 init()
 
+###########################################################
+#User defined functions go here. Use two spaces after each function.
 def print_vector_residues(bool_vec):
+    """ Loop through the boolean vector entries to convert True values
+        to user-friendly residue numbers. """
     residue_list = []
     for i in range(1, len(bool_vec)+1):
         if bool_vec[i] == 1:
@@ -11,7 +15,7 @@ def print_vector_residues(bool_vec):
     print("The residues belonging to the set are: {}".format(residue_list))
 
 
-
+###########################################################
 # import and clean pdb (superantigen + TCR + MHC)
 from pyrosetta.toolbox import cleanATOM
 cleanATOM("2ICW_MHC.pdb")
@@ -22,7 +26,7 @@ pose = pose_from_pdb("2ICW_MHC.clean.pdb")
 fa_starting = Pose()
 fa_starting.assign(pose)
 
-#cone for the analyses
+#clone for the analyses
 fa_working = fa_starting.clone()
 
 #find the pose numbering from pdb numbering
@@ -33,7 +37,6 @@ end_residue = fa_working.pdb_info().pdb2pose('D', 72)
 antigen_helix_selector = pyrosetta.rosetta.core.select.residue_selector.ResidueIndexSelector()
 antigen_helix_selector.set_index("{}-{}".format(start_residue, end_residue))
 antigen_helix_residue_vector = antigen_helix_selector.apply(fa_working)
-#print_vector_residues(antigen_helix_residue_vector)
 
 #find the neighbors
 antigen_helix_neighbor_selector = pyrosetta.rosetta.core.select.residue_selector.NeighborhoodResidueSelector(antigen_helix_residue_vector, 8, True)
@@ -41,8 +44,22 @@ antigen_helix_neighbors = antigen_helix_neighbor_selector.apply(fa_working)
 
 #define the chain H residues belonging to the protein
 H_selector = pyrosetta.rosetta.core.select.residue_selector.ChainSelector('H')
+DF_selector = pyrosetta.rosetta.core.select.residue_selector.ChainSelector('D,E,F')
 
-combine_selectors = pyrosetta.rosetta.core.select.residue_selector.AND_combine(antigen_helix_neighbor_selector, H_selector)
-H_residues = combine_selectors.apply(fa_working)
-print_vector_residues(H_residues)
-#will use and_combine to chose within range and in chain h
+#combine the chain selectors with the region selectors
+combine_H_selectors = pyrosetta.rosetta.core.select.residue_selector.AND_combine(antigen_helix_neighbor_selector, H_selector)
+H_residues_to_design = combine_H_selectors.apply(fa_working)
+combine_DF_selectors = pyrosetta.rosetta.core.select.residue_selector.AND_combine(antigen_helix_neighbor_selector, DF_selector)
+antigen_residues_to_repack = combine_DF_selectors.apply(fa_working)
+
+""" Need to check for numbering information for the values calculated above,
+    I just worked to get the right selectors rather than the right results. """
+
+""" We need a section for loop design to patch the missing residues 
+    on the surface of the antigen """
+
+""" We need a section to set the task factories to design the protein 
+    and to repack the antigen interface """
+
+""" We may add a section to do alanine scanning before the protein residues
+    are subjected to the design algorithm """
